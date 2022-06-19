d9679432e674c00cbc9d565c86d701e8652cf306
mark-fogle <mark.fogle@theactigraph.com>
2022-06-18 20:20:10 -0500

    Added Startup tests and refactored startup.

--- src/DurableFunctionsSlackDemo/Startup.cs
+++ src/DurableFunctionsSlackDemo/Startup.cs
@@ -10,11 +10,8 @@ public class Startup : FunctionsStartup
     public override void Configure(IFunctionsHostBuilder builder)
     {
         var configuration = builder.GetContext().Configuration;
-        var slackApprovalServiceOptions = new SlackApprovalServiceOptions();
-        configuration.GetSection(nameof(SlackApprovalServiceOptions))
-            .Bind(slackApprovalServiceOptions);
-        builder.Services.AddSingleton(slackApprovalServiceOptions);
-        builder.Services.AddHttpClient<ISlackApprovalService>();
-        builder.Services.AddScoped<ISlackApprovalService, SlackApprovalService>();
+        builder.Services.AddSingleton(configuration.GetSection(nameof(SlackApprovalServiceOptions))
+            .Get<SlackApprovalServiceOptions>());
+        builder.Services.AddHttpClient<ISlackApprovalService, SlackApprovalService>();
     }
 }
---
+++ src/DurableFunctionsSlackDemoTests/StartupTests.cs
@@ -0,0 +1,47 @@
+﻿using DurableFunctionsSlackDemo;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace DurableFunctionsSlackDemoTests
+{
+    public class StartupTests
+    {
+        private readonly IHost _host;
+
+        public StartupTests()
+        {
+            _host = new HostBuilder()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddInMemoryCollection(
+                        new [] { 
+                            new KeyValuePair<string, string>("SlackApprovalServiceOptions:SlackWebhookUrl", "None") 
+                        }
+                    );
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<SlackApprovalFunctions>();
+                })
+                .ConfigureWebJobs((context, builder) =>
+                {
+                    new Startup().Configure(new WebJobsBuilderContext
+                    {
+                        Configuration = context.Configuration
+                    }, builder);
+                })
+                .Build();
+        }
+
+        [Fact]
+        public void ServicesShouldBeAbleToResolveSlackApprovalFunctions()
+        {
+            _host.Services.GetRequiredService<SlackApprovalFunctions>();
+        }
+
+        
+    }
+}
